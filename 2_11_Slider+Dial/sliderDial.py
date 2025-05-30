b74fcea045c3bafee65e3e2397624318c9eed08a
5igu <88yuou@gmail.com>
2025-05-30 14:45:46 +0900

    dial 추가 ,, 작동 방식은 같음

--- 2_11_Slider+Dial/sliderDial.py
+++ 2_11_Slider+Dial/sliderDial.py
@@ -20,12 +20,19 @@ class WindowClass(QMainWindow, form_class) :
         self.horizontalSlider_Test.valueChanged.connect(self.showHorizontalSliderValue)
         self.horizontalSlider_Test.rangeChanged.connect(self.printRangedChanged)
 
+        #dial
+        self.dial_Test.valueChanged.connect(self.showDialValue)
+        self.dial_Test.rangeChanged.connect(self.printRangedChanged)
+
         #button
         self.btn_VInfo.clicked.connect(self.getVerticalInfo)
         self.btn_VRange.clicked.connect(self.setVertical)
         
         self.btn_HInfo.clicked.connect(self.getHorizontalInfo)
         self.btn_HRange.clicked.connect(self.setHorizontal)
+
+        self.btn_DInfo.clicked.connect(self.getDialInfo)
+        self.btn_DRange.clicked.connect(self.setDial)
 
 
     def printRangedChanged(self) :
@@ -45,7 +52,7 @@ class WindowClass(QMainWindow, form_class) :
         # self.verticalSlider_Test.setMaximum(500)
         # self.verticalSlider_Test.setMinimum(-500)
         # printRangedChanged에서 "Range Changed"가 두 번 출력되어서 Range로 한 번만 출력되도록 변경
-        
+
         self.verticalSlider_Test.setRange(-500, 500) 
         self.verticalSlider_Test.setPageStep(100)  # Page Up/Down으로 이동할 수 있는 값
         self.verticalSlider_Test.setSingleStep(20)  # Slider 조작이나 방향키로 이동할 수 있는 값
@@ -66,6 +73,21 @@ class WindowClass(QMainWindow, form_class) :
         self.horizontalSlider_Test.setPageStep(100)  # Page Up/Down으로 이동할 수 있는 값
         self.horizontalSlider_Test.setSingleStep(20)  # Slider 조작이나 방향키로 이동할 수 있는 값
 
+
+    # Dial
+    def showDialValue(self) :
+        self.labelDial.setText(str(self.dial_Test.value()))
+
+    def getDialInfo(self) :
+        print("Maximum: " + str(self.dial_Test.maximum()))        # 99
+        print("Minimum: " + str(self.dial_Test.minimum()))        # 0
+        print("PageStep: " + str(self.dial_Test.pageStep()))      # 10
+        print("SingleStep: " + str(self.dial_Test.singleStep()))  # 1
+
+    def setDial(self) :
+        self.dial_Test.setRange(-500, 500)
+        self.dial_Test.setPageStep(100)
+        self.dial_Test.setSingleStep(20)
 
 if __name__ == "__main__" :
     app = QApplication(sys.argv)
