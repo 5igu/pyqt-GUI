25f60d2813adadb1e79f862f6c883f3cae8a2cc3
5igu <88yuou@gmail.com>
2025-05-30 16:41:46 +0900

    입력받은 datetime 값으로 수정 및 포맷 변경
    setLayout()이 중복되면 UI 출력이 깨진다 ..

--- 2_12_DateTimeEdit/datetimeedit.py
+++ 2_12_DateTimeEdit/datetimeedit.py
@@ -18,7 +18,14 @@ class WindowClass(QMainWindow, form_class) :
         self.dateTimeEdit_Test.setDateTime(self.currentDateTime)
 
         # button
-        self.btn_displayDateTime.clicked.connect(self.displayDateTime)
+        self.btn_displayInfo.clicked.connect(self.displayDateTime)
+        self.btn_dateTime.clicked.connect(self.enterDateTimeFunc)
+        self.btn_date.clicked.connect(self.enterDateFunc)
+        self.btn_time.clicked.connect(self.enterTimeFunc)
+        self.btn_change.clicked.connect(self.changeFormat)
+        self.btn_show.clicked.connect(self.showRangeFunc)
+        self.btn_editMax.clicked.connect(self.editMaximum)
+        self.btn_editMin.clicked.connect(self.editMinimum)
 
 
     def displayDateTime(self) :  
@@ -26,12 +33,56 @@ class WindowClass(QMainWindow, form_class) :
     # 사용자가 선택한 값을 꺼내와서 사용하기 위함
     # 가독성, 재사용성, 유지보수
         self.displayDateTimeVar = self.dateTimeEdit_Test.dateTime()
-        self.lbl_displayDateTime.setText(self.displayDateTimeVar.toString("yyyy-MM-dd AP hh:mm:ss"))
+        self.displayDateVar = self.dateTimeEdit_Test.date()
+        self.displayTimeVar = self.dateTimeEdit_Test.time()
+
+        # 각 라벨에 값 표시
+        self.lbl_displayDateTime.setText(self.displayDateTimeVar.toString("yyyy-MM-dd hh:mm:ss"))
+        self.lbl_displayDate.setText(self.displayDateVar.toString("yyyy-MM-dd"))
+        self.lbl_displayTime.setText(self.displayTimeVar.toString("hh:mm:ss"))
+
+    # LineEdit에서 가져온 데이터를 QDateTime 객체로 만들고 DateTimeEdit에 적용
+    def enterDateTimeFunc(self) :
+        self.enterDateTimeText = self.lineEditDateTime.text()
+        self.enterDateTimeVar = QDateTime.fromString(self.enterDateTimeText, "yyyy-MM-dd hh:mm:ss")
+        self.dateTimeEdit_Test.setDateTime(self.enterDateTimeVar)
+    
+    def enterDateFunc(self) :
+        self.enterDateText = self.lineEditDate.text()
+        self.enterDateVar = QDate.fromString(self.enterDateText, "yyyy-MM-dd")
+        self.dateTimeEdit_Test.setDate(self.enterDateVar)
+
+    def enterTimeFunc(self) :
+        self.enterTimeText = self.lineEditTime.text()
+        self.enterTimeVar = QTime.fromString(self.enterTimeText, "hh:mm:ss")
+        self.dateTimeEdit_Test.setTime(self.enterTimeVar)
+
+    def changeFormat(self) :
+        self.displayFormatText = self.lineEditFormat.text()
+        self.dateTimeEdit_Test.setDisplayFormat(self.displayFormatText)
+
+    def showRangeFunc(self) :
+        print(self.dateTimeEdit_Test.minimumDateTime())
+        print(self.dateTimeEdit_Test.maximumDateTime())
+
+    def editMaximum(self) :
+        self.currentMaximumDateTime = self.dateTimeEdit_Test.maximumDateTime()
+        self.currentMaximumDateTime = self.currentMaximumDateTime.addDays(10)
+        self.dateTimeEdit_Test.setMaximumDateTime(self.currentMaximumDateTime)
+    
+    def editMinimum(self) :
+        self.currentMinimumDateTime = self.dateTimeEdit_Test.minimumDateTime()
+        self.currentMinimumDateTime = self.currentMinimumDateTime.addDays(-10)
+        self.dateTimeEdit_Test.setMinimumDateTime(self.currentMinimumDateTime)
 
 
+        
 
 if __name__ == "__main__" :
     app = QApplication(sys.argv)
     myWindow = WindowClass() 
     myWindow.show()
     app.exec()
+
+
+
